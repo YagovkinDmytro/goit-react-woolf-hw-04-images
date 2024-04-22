28b4ed680ef39acb4b6406b674fb3fe79b737694
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-04-22 18:08:30 +0200

    functional App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import { getImageApi } from 'api/dataImages';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -7,116 +7,97 @@ import Button from './Button/Button';
 import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
 
-export class App extends Component {
-  state = {
-    images: [],
-    page: 1,
-    isLoading: false,
-    error: '',
-    searchValue: '',
-    loadMore: false,
-    isEmpty: false,
-    openModal: false,
-    largeImageURL: '',
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+  const [loadMore, setLoadMore] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [tagsImages, setTagsImages] = useState('');
 
-  componentDidUpdate(_, prevState) {
-    if (
-      this.state.searchValue !== prevState.searchValue ||
-      this.state.page !== prevState.page
-    ) {
-      this.getSearchImages();
-    }
-  }
+  useEffect(() => {
+    const getSearchImages = async () => {
+      try {
+        setIsLoading(true);
+        setErrorMessage('');
+        const data = await getImageApi(searchValue, page);
+        const { totalHits, hits } = data;
+        if (hits.length === 0) {
+          setIsEmpty(true);
+          setLoadMore(false);
+          return;
+        }
+        setImages(prev => [...prev, ...hits]);
 
-  getSearchImages = async () => {
-    try {
-      this.setState({ isLoading: true, error: '' });
-      const data = await getImageApi(this.state.searchValue, this.state.page);
-      const { totalHits, hits } = data;
-      if (hits.length === 0) {
-        this.setState({
-          isEmpty: true,
-          loadMore: false,
-        });
-        return;
+        setLoadMore(page < Math.ceil(totalHits / 12));
+        setIsLoading(false);
+        setIsEmpty(false);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
       }
-      this.setState(prev => ({
-        images: [...prev.images, ...hits],
-        loadMore: this.state.page < Math.ceil(totalHits / 12),
-        isLoading: false,
-        isEmpty: false,
-      }));
-    } catch (error) {
-      this.setState({ error: error.message });
-    } finally {
-      this.setState({ isLoading: false });
+    };
+    if (!searchValue) {
+      return;
     }
-  };
+    getSearchImages();
+  }, [searchValue, page]);
 
-  searchParam = searchValue => {
-    if (searchValue === '') {
+  const searchParam = searchParam => {
+    if (searchParam === '') {
       alert('Please type a new word');
       return;
     }
-    if (searchValue === this.state.searchValue) {
+    if (searchParam === searchValue) {
       alert('We have already shown thoes photos');
       return;
     }
-    this.setState({ searchValue, images: [], page: 1 });
+    setSearchValue(searchParam);
+    setImages([]);
+    setPage(1);
   };
 
-  handleAddPage = () => {
-    this.setState(prev => ({ page: prev.page + 1 }));
+  const handleAddPage = () => {
+    setPage(prev => prev + 1);
   };
 
-  handleSetOpenModal = (largeImageURL, tags) => {
-    this.setState({
-      largeImageURL,
-      tags,
-      openModal: true,
-    });
+  const handleSetOpenModal = (largeImageURL, tags) => {
+    setLargeImageURL(largeImageURL);
+    setTagsImages(tags);
+    setOpenModal(true);
   };
 
-  handleSetCloseModal = () => {
+  const handleSetCloseModal = () => {
     this.setState({
       openModal: false,
     });
   };
 
-  render() {
-    const {
-      images,
-      isLoading,
-      error,
-      loadMore,
-      isEmpty,
-      openModal,
-      largeImageURL,
-      tags,
-    } = this.state;
-
-    return (
-      <div className="App">
-        {isLoading && <Loader></Loader>}
-        <Searchbar searchParam={this.searchParam} />
-        <ImageGallery>
-          <ImageGalleryItem
-            imagesArr={images}
-            handleSetOpenModal={this.handleSetOpenModal}
-          ></ImageGalleryItem>
-        </ImageGallery>
-        {error && <h1>{error}</h1>}
-        {isEmpty && <h1>Sorry. There are no inages.😥</h1>}
-        {loadMore && <Button handleAddPage={this.handleAddPage}></Button>}
-        {openModal && (
-          <Modal
-            largeImageURL={largeImageURL}
-            tags={tags}
-            handleSetCloseModal={this.handleSetCloseModal}
-          ></Modal>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      {isLoading && <Loader></Loader>}
+      <Searchbar searchParam={searchParam} />
+      <ImageGallery>
+        <ImageGalleryItem
+          imagesArr={images}
+          handleSetOpenModal={handleSetOpenModal}
+        ></ImageGalleryItem>
+      </ImageGallery>
+      {errorMessage && <h1>{errorMessage}</h1>}
+      {isEmpty && <h1>Sorry. There are no inages.😥</h1>}
+      {loadMore && <Button handleAddPage={handleAddPage}></Button>}
+      {openModal && (
+        <Modal
+          largeImageURL={largeImageURL}
+          tags={tagsImages}
+          handleSetCloseModal={handleSetCloseModal}
+        ></Modal>
+      )}
+    </div>
+  );
+};
